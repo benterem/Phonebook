6235d242982dce15e7fbf1a430d1977d37318830
Ben Terem <benterem@gmail.com>
2021-06-01 12:39:52 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const { request, response } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
+
+
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Phonebook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    
+    response.send(
+      `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+      `
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id);
+    const person = persons.find(p => p.id === id)
+    console.log(person)
+    if(person){
+        response.json(person)
+    } else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+
+    console.log(request.body)
+    const body = request.body
+    console.log(body)
+
+
+    if(!(body.name && body.number)){
+        return response.status(400).json(
+            { error: 'Contacts must have name and number' }
+        )
+    } else if(persons.find(person => person.name === body.name)){
+        return response.status(400).json(
+            { error: `${body.name} is aleardy stored on the server` }
+        )
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000000)
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.put('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const body = request.body
+
+    persons = persons.filter(p => p.id !== id)
+    persons = persons.concat(body)
+    response.json(body)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
